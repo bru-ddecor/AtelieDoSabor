751afd60a4e35c6f614ff25282de2a66648e27c9
Bruna <bru.tironiol@gmail.com>
2026-05-27 00:21:37 -0300

    Exclusão de arquivos duplicados

--- login.js
+++
@@ -1,7 +0,0 @@
-const container = document.getElementById('container');
-const cadastrarButton = document.querySelector('registro');
-const entrarButton = document.querySelector('login');
-
-cadastrarButton.addEventListener('click', () => {
-    container.classList.add('active');
-});
